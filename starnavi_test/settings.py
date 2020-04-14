cd61a9992be645a630ad0bda7f440262ac3ec533
GrytsenkoDaria <grytsenkodaria@gmail.com>
2020-04-14 20:43:30 +0300

    Additional changes

--- starnavi_test/settings.py
+++ starnavi_test/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from environs import Env
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+env = Env()
+env.read_env()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zxamd!&cn^ch^v59n%gjmj(cu)ah3$ptc1$kj*k7rq(2!l6!x-'
+SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
